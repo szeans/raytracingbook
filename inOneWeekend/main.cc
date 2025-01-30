0972c08065df814f1088bf085093c361add6871d
Sean Choi <szeanchoi@gmail.com>
2025-01-29 23:48:30 -0600

    vec and color helpers;

--- inOneWeekend/main.cc
+++ inOneWeekend/main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 using namespace std;
@@ -13,16 +16,14 @@ int main() {
   cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int i = 0; i < image_height; i++) {
+    clog << "/rScanlines remaining: " << (image_height - i) << ' ' << flush;
     for (int j = 0; j < image_width; j++) {
-      auto r = double(j) / (image_width - 1);
-      auto g = double(i) / (image_height - 1);
-      auto b = 0.0;
+      auto pixel_color = color(double(i) / (image_width - 1),
+                               double(j) / (image_height - 1), 0.0);
 
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      cout << ir << ' ' << ig << ' ' << ib << '\n';
+      write_color(cout, pixel_color);
     }
   }
+
+  clog << "\rDone.              \n";
 }
